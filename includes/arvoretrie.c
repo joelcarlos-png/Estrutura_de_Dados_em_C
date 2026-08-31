134473938cff5f0b141a9824ccc7d21741171254
Joel Carlos Assunção Padilha <2025105231940015@ifgoiano.local>
2026-08-31 16:47:05 -0300

    aaaaaaaaaaaa

--- includes/arvoretrie.c
+++ includes/arvoretrie.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <stdbool.h>
 #include "arvoretrie.h"
@@ -21,16 +21,39 @@ void inserir(No **raiz, char *palavra){
     inserir((*raiz)->letras[i], palavra + 1);
 }
 
-void excluir(No **raiz, char *palavra){
-    int i = palavra[0] - 'a';
-    if(palavra[1] != '\0') excluir((*raiz)->letras[i], palavra + 1);
-    bool vazio = true;
+bool verifFrenteVazio(No *raiz[]){
     for(int in = 0; in < 26; in++){
-        if((*raiz)->letras[i]->letras[in] != NULL) vazio = false;
+        if((*raiz)->letras[in] != NULL) return true;
     }
-    if(!(*raiz)->letras[i]->palavra);
-    if((*raiz)->letras[i]->palavra) ;
+    return false;
+}
 
+static void excluirParci(No **raiz, char *palavraParci, No *ultimofim){
+    if((*raiz) == ultimofim) return;
+    No *pai;
+    for(int i = 0; palavraParci[2] != '\0'; i ++){
+        pai = ultimofim->letras[palavraParci[i] - 'a'];
+    }
+}
+
+void excluir(No **raiz, char *palavra){
+    No *ultimofim;
+    char *palavraParci;
+    int i = palavra[0] - 'a';
+    if(palavra[0] == '\0') return;
+    if(palavra[1] == '/0'){
+        if(verifFrenteVazio(raiz)){
+            (*raiz)->palavra = false;
+            return;
+        }
+        
+
+    }
+    if((*raiz)->palavra == true){
+        ultimofim = raiz;
+        palavraParci = palavra;
+    }
+    excluir((*raiz)->letras[i], palavra + 1);
 }
 
 void procurar(No *raiz);
--- includes/arvoretrie.h
+++ includes/arvoretrie.h
@@ -4,11 +4,13 @@
 #include <stdbool.h>
 
 typedef struct no{
-    struct no *letras[26];
+    struct no *letras[26]; 
     bool palavra;
 }No;
 
 No *criarNo();
+
+bool verifFrenteVazio(No *raiz[]);
 
 void inserir(No **raiz, char *palavra);
 void procurar(No **raiz, char *palavra);
